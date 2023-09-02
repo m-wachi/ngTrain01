b1c3ac15e3cca3b1561354741b4411386a260c5a
m-wachi <m-wachi@debian01>
2023-09-03 01:15:31 +0900

    change base href.

--- ngTrain01/src/app/app.module.ts
+++ ngTrain01/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/ngTrain01'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
