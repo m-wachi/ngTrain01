f784291def775c8b3bd8bb02d615a0f1f08c2ec6
m-wachi <m-wachi@debian01>
2023-09-03 02:44:14 +0900

    add routing

--- ngTrain01/src/app/app.module.ts
+++ ngTrain01/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/ngTrain01'}],
   bootstrap: [AppComponent]
