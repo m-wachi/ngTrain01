ea77df0ff4f729e795c89eb4fd8542357f16f840
m-wachi <xxxx@yyy.zz.com>
2023-09-05 13:58:59 +0900

    ng add @angular/material

--- ngTrain01/src/app/app.component.ts
+++ ngTrain01/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+//import {MatIconModule} from '@angular/material/icon';
+//import {MatDividerModule} from '@angular/material/divider';
+//import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //standalone: true,
+  //imports: [MatButtonModule, MatDividerModule, MatIconModule]
 })
 export class AppComponent {
   title = 'ngTrain01';
--- ngTrain01/src/app/app.module.ts
+++ ngTrain01/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/ngTrain01'}],
   bootstrap: [AppComponent]
